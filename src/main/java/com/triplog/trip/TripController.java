96e88021bd4dc0ffda6342c0f3dea09c5a03f73f
minyoung13 <mypark0113@naver.com>
2025-05-21 15:43:57 +0900

    style: 엔드포인트 수정

--- src/main/java/com/triplog/trip/TripController.java
+++ src/main/java/com/triplog/trip/TripController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/trips")
 @RequiredArgsConstructor
 @Slf4j
 @Tag(name = "Trip", description = "Trip 관련 API입니다.")
@@ -20,7 +20,7 @@ public class TripController {
 
     private final TripService tripService;
 
-    @PostMapping("/users/trips")
+    @PostMapping
     @Operation(summary = "여행 생성", description = "여행에 대한 정보를 입력받고 새로 생성합니다.")
     public ResponseEntity<TripCreateResponse> createTrip(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody @Valid TripCreateRequest request) {
         String username = userDetails.getUsername();
@@ -28,7 +28,7 @@ public class TripController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/users/trips")
+    @GetMapping
     @Operation(summary = "여행 목록 조회", description = "사용자의 여행 목록을 조회합니다.")
     public ResponseEntity<TripFindByUserResponse> getTripsByUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
         String username = userDetails.getUsername();
@@ -36,14 +36,14 @@ public class TripController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/trip/{tripId}")
+    @GetMapping("{tripId}")
     @Operation(summary = "여행 상세 조회", description = "각 여행의 상세 정보를 조회합니다.")
     public ResponseEntity<TripDetailResponse> getTripDetail(@PathVariable Long tripId) {
         TripDetailResponse response = tripService.getTripDetail(tripId);
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/trip/{tripId}/invite")
+    @PostMapping("{tripId}/invite")
     @Operation(summary = "동반자 초대", description = "사용자의 닉네임으로 여행에 동반자를 초대합니다.")
     public ResponseEntity<String> inviteTrip(@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable Long tripId, @RequestBody @Valid TripInviteRequest request) {
         String username = userDetails.getUsername();
@@ -51,7 +51,7 @@ public class TripController {
         return ResponseEntity.ok("동반자 초대 완료");
     }
 
-    @GetMapping("/users/invite")
+    @GetMapping("/invite")
     @Operation(summary = "여행 초대 조회", description = "사용자가 받은 여행 초대를 조회합니다.")
     public ResponseEntity<TripInviteFindByUserResponse> getTripInvitesByUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
         String username = userDetails.getUsername();
