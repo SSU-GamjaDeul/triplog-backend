7830bbfb44333873f1eb69ffa8ff3619627f40b8
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-05-24 14:11:41 +0900

    test: health check

---
+++ src/main/java/com/triplog/common/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.triplog.common;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Hidden
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> HealthCheck() {
+        return ResponseEntity.ok("Health Check!");
+    }
+}
