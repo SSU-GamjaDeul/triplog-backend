a727c6ca5a7f8193462f975c7e0fbf8a2c3e1033
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-05-25 08:31:48 +0900

    docs: 수정 예정 API - Swagger 숨김 처리

--- src/main/java/com/triplog/record/controller/RecordController.java
+++ src/main/java/com/triplog/record/controller/RecordController.java
@@ -24,7 +24,7 @@ public class RecordController {
     private final RecordService recordService;
 
     @GetMapping("/place")
-    @Operation(summary = "장소 기반 기록 목록 조회", description = "카카오 장소 id를 기반으로 해당 장소에 대한 모든 유저의 공개된 기록 목록을 조회합니다.")
+    @Operation(hidden = true, summary = "장소 기반 기록 목록 조회", description = "카카오 장소 id를 기반으로 해당 장소에 대한 모든 유저의 공개된 기록 목록을 조회합니다.")
     public ResponseEntity<RecordFindAllByPlaceResponse> getRecordsByPlace(@RequestParam Long kakaoPlaceId) {
 
         RecordFindAllByPlaceResponse response = recordService.getRecordsByPlace(kakaoPlaceId);
@@ -55,7 +55,7 @@ public class RecordController {
     }
 
     @GetMapping("/location")
-    @Operation(summary = "위치 기반 기록 목록 조회 (카테고리 필터 포함)", description = "위도·경도 범위와 선택한 카테고리에 해당하는 장소들을 기준으로 사용자의 기록 목록을 조회합니다.")
+    @Operation(hidden = true, summary = "위치 기반 기록 목록 조회 (카테고리 필터 포함)", description = "위도·경도 범위와 선택한 카테고리에 해당하는 장소들을 기준으로 사용자의 기록 목록을 조회합니다.")
     public ResponseEntity<RecordFindAllByLocationResponse> getRecordsByLocation(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                                                 @RequestParam double minLat,
                                                                                 @RequestParam double maxLat,
